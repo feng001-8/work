4fbd14d5f89d45b22d6bdc80c929a8001d6caf4c
feng001-8 <114282544+feng001-8@users.noreply.github.com>
2025-07-14 16:22:36 +0800

    feat: 添加基础ERC20代币合约实现

---
+++ src/ERC20.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract BaseERC20 {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowances;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() {
+        name = "BaseERC20";
+        symbol = "ZWG";
+        decimals = 18;
+        totalSupply = 1000000000000000000000000;
+        balances[msg.sender] = totalSupply;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balances[msg.sender] >= _value, "insufficient-balance");
+        
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(balances[_from] >= _value, "insufficient-balance");
+        
+        if (_from != msg.sender && allowances[_from][msg.sender] != type(uint256).max) {
+            require(allowances[_from][msg.sender] >= _value, "insufficient-allowance");
+            allowances[_from][msg.sender] -= _value;
+        }
+        
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowances[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowances[_owner][_spender];
+    }
+}
